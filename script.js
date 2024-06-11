867e9ff7dc76b347b74ee1d6778e9edc91c2230d
Nikolay Tuzov <just.skiv@gmail.com>
2024-06-11 17:54:03 +0500

    add english version

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 function toggleDetails(element) {
     const details = element.parentElement.nextElementSibling;
+    const lang = document.documentElement.lang; // Получаем текущий язык страницы
+    let moreText, lessText;
+
+    if (lang === 'ru') {
+        moreText = 'подробнее';
+        lessText = 'скрыть';
+    } else {
+        moreText = 'more';
+        lessText = 'less';
+    }
+
     if (details.style.maxHeight && details.style.maxHeight !== '0px') {
         details.style.maxHeight = '0';
-        element.textContent = 'подробнее';
+        element.textContent = moreText;
         details.classList.remove('show');
     } else {
         details.style.maxHeight = details.scrollHeight + 'px';
-        element.textContent = 'скрыть';
+        element.textContent = lessText;
         details.classList.add('show');
     }
 }
